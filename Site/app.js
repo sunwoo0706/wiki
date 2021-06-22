9f23c89ca18a156cbf9416c4ab6d16ef27e7bd7a
Goolgae <sunwoo0706@icloud.com>
2021-06-22 16:44:51 +0900

    [UPDATE] rename Sites -> Site by sunwoo0706

--- Site/app.js
+++ Site/app.js
@@ -22,6 +22,7 @@ const htmlElements = `
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="Sites/Styles/Style.css">
       <title>MH-frontend-roadmap</title>
   </head>
   <body>
